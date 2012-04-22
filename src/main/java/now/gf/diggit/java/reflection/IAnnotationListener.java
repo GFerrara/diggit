69f249d337121aab38412a4ff3e3158b1c5a1fc4
Giorgio Ferrara <antares.email@gmail.com>
2012-04-23 01:52:55 +0200

    Javadoc

--- src/main/java/now/gf/diggit/java/reflection/IAnnotationListener.java
+++ src/main/java/now/gf/diggit/java/reflection/IAnnotationListener.java
@@ -4,10 +4,22 @@ import java.lang.annotation.Annotation;
 import java.util.Map;
 
 /**
+ * <p>There is no standard, unique, way to extract info from a Java annotation,
+ * therefore, for diving into annotations, we have to introduce classes that act
+ * as <i>annotation listeners</i> so that, everytime an annotation is reached, 
+ * the associated listener is called. This listener, in turn, provides the caller 
+ * with a map of attributes (if any) that are bound to that specific annotation.</p>  
  * 
  * @author Giorgio Ferrara
+ * @see Annotation
  *
  */
 public interface IAnnotationListener <T extends Annotation> {
+	/**
+	 * Returns a map of annotation attributes or a null value if annotation doesn't expose attributes. 
+	 * 
+	 * @param annotation a Java annotation
+	 * @return a map of annotation attributes (key=<annotation attribute name>, value=<annotation attribute value>)
+	 */
 	public Map<String, Object> getAttributesMap(T annotation);
 }
